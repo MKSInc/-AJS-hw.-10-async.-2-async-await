343ee039efad5ebdd8c2ea40174bbb27812e7750
Maksim <pyshkov.maksim@gmail.com>
2020-07-27 23:38:45 +0300

    Решение для третьей задачи

--- src/js/GameSavingLoader.js
+++ src/js/GameSavingLoader.js
@@ -5,7 +5,7 @@ import GameSaving from './GameSaving';
 export default class GameSavingLoader {
   static load() {
     return read()
-      .then((result) => json(result))
-      .then((result) => new GameSaving(JSON.parse(result)));
+      .then((result) => { console.log(result); json(result); })
+      .then((result) => { console.log(result); return new GameSaving(JSON.parse(result)); });
   }
 }
--- src/js/__tests__/GameSavingLoader.test.js
+++ src/js/__tests__/GameSavingLoader.test.js
@@ -1,14 +1,23 @@
+/* eslint-disable max-len */
 import GameSavingLoader from '../GameSavingLoader';
+import read from '../read';
+import json from '../json';
 
-const saving = {
-  id: 9,
-  created: 1546300800,
-  userInfo: {
-    id: 1,
-    name: 'Hitman',
-    level: 10,
-    points: 2000,
-  },
-};
+jest.mock('../read');
+jest.mock('../json');
 
-test('The load() method must return a promise with the correct object', () => expect(GameSavingLoader.load()).resolves.toEqual(saving));
+beforeEach(() => {
+  jest.resetAllMocks();
+});
+
+test('The load() method must return a promise in a state \'fulfilled\'', async () => {
+  read.mockReturnValue(new Promise((resolve, rejected) => resolve({})));
+  json.mockReturnValue(new Promise((resolve, rejected) => resolve('{}')));
+  expect(GameSavingLoader.load()).resolves.toEqual({ id: undefined, created: undefined, undefined });
+});
+
+test('The load() method must return a promise in a state \'rejected\'', () => {
+  read.mockReturnValue(new Promise((resolve, rejected) => { throw new Error('Some error'); }));
+  json.mockReturnValue(async () => '{}');
+  expect(GameSavingLoader.load()).rejects.toThrow('Some error');
+});
